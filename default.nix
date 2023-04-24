cce4e92a084e0c4990536b75cfc37a86958ef51e
Patrick Aldis <patrick.aldis@virginmedia.com>
2023-04-24 15:41:46 +0100

    pinned nixpkgs

--- default.nix
+++ default.nix
@@ -1,6 +1,11 @@
 # default.nix
 let
-  pkgs = import <nixpkgs> { };
+  pkgs = import (builtins.fetchTarball {
+    name = "nixpkgs-unstable";
+    url = "https://github.com/nixos/nixpkgs/archive/4a5817c44a62c415ce3ec24ff832d12108805012.tar.gz";
+    sha256 = "1s0dgaywwk0rlcj7n2a32w0izm91wi3arsxq2ymxr4fsb9asf1yi";
+  })
+  { };
 in
   pkgs.haskellPackages.developPackage {
     root = ./.;
