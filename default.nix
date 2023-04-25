db2b35d3d6bee47a7095810aa675a629d8c92e5f
Patrick Aldis <patrick.aldis@virginmedia.com>
2023-04-25 21:24:10 +0100

    turned into hix

--- default.nix
+++ default.nix
@@ -9,9 +9,9 @@ let
 in
   pkgs.haskellPackages.developPackage {
     root = ./.;
-    modifier = drv:
-      pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
-        [ cabal-install
-          ghcid
-        ]);
+    # modifier = drv:
+    #   pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
+    #     [ cabal-install
+    #       ghcid
+    #     ]);
   }
