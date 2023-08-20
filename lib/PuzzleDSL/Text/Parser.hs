deb86136d68805cc3064472793d4ea3498b229c4
Patrick Aldis <patrick.aldis@virginmedia.com>
2023-08-20 14:40:08 +0100

    added examples for text format

--- lib/PuzzleDSL/Text/Parser.hs
+++ lib/PuzzleDSL/Text/Parser.hs
@@ -3,8 +3,16 @@ module PuzzleDSL.Text.Parser where
 import Data.Void
 import PuzzleDSL.Internal.Spec (PuzzleClass (..))
 import Text.Megaparsec
+import Text.Megaparsec.Char
 
 type Parser = Parsec Void String
 
 puzzleParser :: Parser String
-puzzleParser = pure "hey"
+puzzleParser = do
+  space
+  _ <- string "---"
+  jsonBlock <- takeWhileP Nothing (/= '-')
+  _ <- string "---"
+  space
+  ruleBlock <- takeWhileP Nothing (const True)
+  pure (jsonBlock ++ " |||| " ++ ruleBlock)
