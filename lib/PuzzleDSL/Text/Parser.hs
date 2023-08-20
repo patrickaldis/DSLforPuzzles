b821f56fdd17f28102a2e5da54effb6450f7aaae
Patrick Aldis <patrick.aldis@virginmedia.com>
2023-08-20 11:23:15 +0100

    testing for parsing

--- lib/PuzzleDSL/Text/Parser.hs
+++ lib/PuzzleDSL/Text/Parser.hs
@@ -1,1 +1,10 @@
 module PuzzleDSL.Text.Parser where
+
+import Data.Void
+import PuzzleDSL.Internal.Spec (PuzzleClass (..))
+import Text.Megaparsec
+
+type Parser = Parsec Void String
+
+puzzleParser :: Parser String
+puzzleParser = pure "hey"
